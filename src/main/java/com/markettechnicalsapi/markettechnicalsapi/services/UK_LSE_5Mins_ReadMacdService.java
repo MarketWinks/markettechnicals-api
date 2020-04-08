063665206b84ae3d3e8c95b1713a7fc0d72fef99
broadwise.cxo@gmail.com <broadwise.cxo@gmail.com>
2020-04-09 00:58:29 +0100

    initial commit

--- src/main/java/com/markettechnicalsapi/markettechnicalsapi/services/UK_LSE_5Mins_ReadMacdService.java
+++ src/main/java/com/markettechnicalsapi/markettechnicalsapi/services/UK_LSE_5Mins_ReadMacdService.java
@@ -33,7 +33,7 @@ public class UK_LSE_5Mins_ReadMacdService {
 		try {
 
 			MongoClient mongoClient = MongoClients.create(
-					"mongodb+srv://checkoutfood:checkoutfood123@cluster0-5ffrd.mongodb.net/test?retryWrites=true");
+					"mongodb+srv://marketwinks:L9sS6oOAk1sHL0yi@aws-eu-west1-cluster-tszuq.mongodb.net/marketwinksdbprod?retryWrites=true");
 			MongoDatabase TestDB = mongoClient.getDatabase("test");
 			MongoCollection<org.bson.Document> uk_lse_5mins_livemarketmacdjsonCollection = TestDB
 					.getCollection("uk_lse_5mins_livemarketmacdjson");
